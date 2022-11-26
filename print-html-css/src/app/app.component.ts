c93e3985c4746ea2541d5af423114938c7e50a00
CaoThanhLong <thanhlong2803@hotmail.com>
2022-11-26 11:29:05 +0700

    paging for print

--- print-html-css/src/app/app.component.ts
+++ print-html-css/src/app/app.component.ts
@@ -9,6 +9,23 @@ export class AppComponent {
   title = 'print-html-css';
 
   print() { 
+    this.paging();
+  }
+  paging() {
+    let totalPages = 2;
+    for (let i = 1; i < totalPages; i++) {
+      var pageNumberDiv = document.createElement("div");
+      pageNumberDiv.setAttribute("class", "page_number");
+      var pageNumber = document.createTextNode(i + " / " + totalPages);
+      pageNumberDiv.style.position = "absolute";
+      pageNumberDiv.style.top = "calc((" + i + " * (170mm - 0.5px)) - 30px)";
+      pageNumberDiv.style.height = "16px";
+      pageNumberDiv.style.width = "30px";
+      pageNumberDiv.style.fontSize = "11px";
+      pageNumberDiv.appendChild(pageNumber);
+      document.getElementById('content-pdf')?.appendChild(pageNumberDiv);
+      pageNumberDiv.style.left = "calc(100% - (" + pageNumberDiv.offsetWidth + "px - 10px))";
+    }
     window.print();
   }
 }
